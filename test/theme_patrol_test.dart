15a7392e02f2267310bb66e387c435e66467981f
Irfan Vigma Taufik <davigmacode@gmail.com>
2024-06-06 17:13:14 +0700

    Added dummy test

--- test/theme_patrol_test.dart
+++ test/theme_patrol_test.dart
@@ -1,4 +1,6 @@
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test/flutter_test.dart';
 // import 'package:theme_patrol/theme_patrol.dart';
 
-void main() {}
+void main() {
+  test('dummy test', () {});
+}
